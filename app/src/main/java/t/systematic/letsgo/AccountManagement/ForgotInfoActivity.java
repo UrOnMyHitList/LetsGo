675e03e142d58fc27402c95f7b8b989d9c14690e
flagninja <tristansmn@gmail.com>
2018-03-19 02:02:48 -0700

    Database connectivity for ForgotInfo
    entering username or email now retrieves the relevant information from the database.

--- app/src/main/java/t/systematic/letsgo/AccountManagement/ForgotInfoActivity.java
+++ app/src/main/java/t/systematic/letsgo/AccountManagement/ForgotInfoActivity.java
@@ -3,9 +3,18 @@ package t.systematic.letsgo.AccountManagement;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import t.systematic.letsgo.R;
 
@@ -15,20 +24,69 @@ public class ForgotInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_info);
-
+        final DatabaseReference myDb;
+        myDb = FirebaseDatabase.getInstance().getReference();
         Intent intent = getIntent();
-        String flavor = intent.getStringExtra(LogInActivity.EXTRA_FLAVOR);
-
+        final String flavor = intent.getStringExtra(LogInActivity.EXTRA_FLAVOR);
+        final String TAG = "ForgotInfoActivity";
         TextView text = findViewById(R.id.ForgotLoginTextView);
         text.setText(flavor);
-
-        if (flavor.equals("Enter your username to recover your password:")) {
-            EditText enterBox = findViewById(R.id.enterInfoBox);
-            enterBox.setVisibility(View.VISIBLE);
+        final EditText enterInfoBox = findViewById(R.id.enterInfoBox);
+        final EditText enterEmailBox = findViewById(R.id.enterEmailBox);
+        final String flavorCheck = "Enter your username to recover your password:";
+        if (flavor.equals(flavorCheck)) {
+            enterInfoBox.setVisibility(View.VISIBLE);
         }
         else {
-            EditText enterBox = findViewById(R.id.enterEmailBox);
-            enterBox.setVisibility(View.VISIBLE);
+            enterEmailBox.setVisibility(View.VISIBLE);
         }
+        Button b1 = (Button)findViewById(R.id.forgotLoginButton);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDb.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Boolean found = false;
+                        for (DataSnapshot userList: dataSnapshot.getChildren()) {
+                            String username = userList.getKey();
+                            if (flavor.equals(flavorCheck)) {
+                                if (username.equals(enterInfoBox.getText().toString())) {
+                                    found = true;
+                                    DataSnapshot temp = userList.child("password"); //returns key = password value = "whatever"
+                                    String pw = temp.getValue(String.class);
+                                    Toast toast = Toast.makeText(getApplicationContext(), pw , Toast.LENGTH_LONG);
+                                    toast.show();
+                                    //TODO update this logic with what we actually want to do later
+                                    break;
+                                }
+                            }
+                            else {
+                                DataSnapshot temp = userList.child("email");
+                                String userEmail = temp.getValue(String.class);
+                                if (userEmail.equals(enterEmailBox.getText().toString())) {
+                                    found = true;
+                                    Toast toast = Toast.makeText(getApplicationContext(), username, Toast.LENGTH_LONG);
+                                    toast.show();
+                                    //TODO update this logic with what we actually want to do later
+                                    break;
+                                }
+                            }
+                        }
+                        if (!found) {
+                            String toastText = "No such User in System";
+                            Toast toast = Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "loadUsers:onCancelled", databaseError.toException());
+                    }
+                });
+            }
+        });
     }
 }
